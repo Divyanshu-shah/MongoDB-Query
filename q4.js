56e989ebb95d9edf42707e2f91f2fdb3fe568f47
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-02 11:42:12 +0530

    updated

--- q4.js
+++ q4.js
@@ -17,3 +17,11 @@ db.employees.find().limit(3)
 db.employees.find().skip(1)
 
 db.employees.find().limit(3).skip(1)
+
+db.employees.find().sort({name:1})  //sort in ascending order
+
+db.employees.find().sort({name:-1}) // sort in descending order
+
+db.employees.find().limit(3).skip(1).sort({name:-1})
+
+db.employees.find().count()
