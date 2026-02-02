6c0b17996f5ed592ba412d85a30e571fdf3c7fdb
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-02-02 12:06:21 +0530

    updated

--- q6.js
+++ q6.js
@@ -5,3 +5,12 @@ db.employees.find(
 db.employees.find(
     {department:"IT",salary:{$gt:2000}}
 )
+
+db.employees.find(
+    {$and:[{department:"IT"},{salary:{$gt:2000}}]}
+)
+
+db.employees.find(
+    {$or:[{department:"IT"},{salary:{$gt:2000}}]},
+    {name:1,email:1,department:1}
+)
